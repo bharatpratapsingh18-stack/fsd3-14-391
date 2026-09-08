833ca8ac6a37e51de29123505ea5de8aaee4cdc9
Bharat Pratap Singh <bharatpratapsingh18@gmail.com>
2026-09-08 09:37:01 +0530

    add teams.js

---
+++ lab3/teams.js
@@ -0,0 +1,39 @@
+let teams = [
+  {
+    id: 1,
+    tname: "Rusty",
+    tl: "Bharat Pratap Singh",
+    email: "bharat18@gmail.com",
+    members: 6,
+  },
+  {
+    id: 2,
+    tname: "Code Crafter",
+    tl: "ManishaSingh",
+    email: "mani.singh@gmail.com",
+    members: 5,
+  },
+];
+let nextId = 3;
+export const getAllTeams = () => teams;
+
+export const getTeamById = (id) => teams.find((team) => team.id === id);
+
+export const addTeam = (newTeam) => {
+  const team = { id: nextId++, newTeam };
+  team.push(team);
+  return team;
+};
+export const updateTeamById = (id, updateTeam) => {
+  const team = getTeamById(id);
+  if (!team) return null;
+  Object.assign(team, updateTeam);
+  return team;
+};
+
+export const deleteTeam = (id) => {
+  const index = teams.findIndex((team) => team.id === id);
+  if (index == -1) return false;
+  teams.splice(index, 1);
+  return true;
+};
