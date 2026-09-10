2402dda24c9feea3e2837dc47f57558b5ce5687c
Bharat Pratap Singh <bharatpratapsingh18@gmail.com>
2026-09-10 14:50:11 +0530

    API

--- lab4/teams.js
+++ lab4/teams.js
@@ -2,28 +2,31 @@ let teams = [
   {
     id: 1,
     tname: "Rusty",
-    tl: "Bharat Pratap Singh",
-    email: "bharat18@gmail.com",
+    tl: "Aashish Raj Singh",
+    email: "ashi.raj@gmail.com",
     members: 6,
   },
   {
     id: 2,
-    tname: "Code Crafter",
-    tl: "ManishaSingh",
+    tname: "Code Crafters",
+    tl: "Manisha Singh",
     email: "mani.singh@gmail.com",
     members: 5,
   },
 ];
+
 let nextId = 3;
+
 export const getAllTeams = () => teams;
 
 export const getTeamById = (id) => teams.find((team) => team.id === id);
 
 export const addTeam = (newTeam) => {
-  const team = { id: nextId++, newTeam };
-  team.push(team);
+  const team = { id: nextId++, ...newTeam };
+  teams.push(team);
   return team;
 };
+
 export const updateTeamById = (id, updateTeam) => {
   const team = getTeamById(id);
   if (!team) return null;
