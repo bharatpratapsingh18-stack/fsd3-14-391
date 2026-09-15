2e8d41b8ab332042c5b25e52d602d6c470bc8695
Bharat Pratap Singh <bharatpratapsingh18@gmail.com>
2026-09-15 10:10:11 +0530

    app with http route completed

--- lab4/app.js
+++ lab4/app.js
@@ -1,6 +1,12 @@
 import http from "http";
 // import * as teams from "teams.js";
-import { getAllTeams, addTeam } from "./teams.js";
+import {
+  getAllTeams,
+  addTeam,
+  getTeamById,
+  deleteTeam,
+  updateTeamById,
+} from "./teams.js";
 import { parse as parseUrl } from "url";
 
 const PORT = 5000;
@@ -46,9 +52,52 @@ const server = http.createServer(async (req, res) => {
     const team = addTeam({ tname, tl, members });
 
     return sendJson(res, 201, team, "Message", "Team registered successfully");
+  } else if (pathname.startsWith("/api/v1/teams/") && method === "GET") {
+    const id = Number(pathname.split("/").pop());
+    const team = getTeamById(id);
+
+    if (!team)
+      return sendJson(res, 400, {
+        erro: `Team with id: ${id} not found`,
+      });
+    return sendJson(res, 200, team, "Message", "Team Found");
+  } else if (pathname.startsWith("/api/v1/teams/") && method === "DELETE") {
+    const id = Number(pathname.split("/").pop());
+    const team = getTeamById(id);
+
+    if (!team)
+      return sendJson(res, 404, {
+        error: `Team with id: ${id} not found`,
+      });
+    deleteTeam(id);
+
+    return sendJson(res, 200, team, "Message", "Team Found");
+  } else if (pathname,startsWith  ("/api/v1/teams/") && method == "PUT") {
+    const id = Number(pathname.split("/").pop());
+    const oldteam = getTeamById(id);
+
+    if (!oldteam)
+      return sendJson(res, 404, {
+        error: `Team with id: ${id} not found`,
+      });
+
+    const { tname, tl, members } = await parseJSONBody(req);
+    if (!tname || !tl || !members)
+      return sendJson(res, 400, {
+        error: "Team Name, Team Leader, or Members not defined",
+      });
+
+    const updateTeam = updateTeamById(id, { tname, tl, members });
+    return sendJson(
+      res,
+      200,
+      updateTeam,
+      "Message",
+      "Team updated successfully",
+    );
   } else {
     res.statusCode = 404;
-    res.end();
+    res.end("Not matching");
   }
 });
 
