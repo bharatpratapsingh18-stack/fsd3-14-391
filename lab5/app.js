0243bbbb1b891c9ee28d22dada91ae9f7f73e051
Bharat Pratap Singh <bharatpratapsingh18@gmail.com>
2026-09-15 10:29:50 +0530

    app

--- lab5/app.js
+++ lab5/app.js
@@ -1,8 +1,11 @@
 import express from 'express'
 
 const app =express();
-app.get("/",(req,res)=>{
+app.get("",(req,res)=>{
     res.send("<h1>Hello Express");
 });
+app.use("*",(req,res)=>{
+    res.send("route not found");
+})
 
 app.listen(3000,()=>console.log("server is Running"));
