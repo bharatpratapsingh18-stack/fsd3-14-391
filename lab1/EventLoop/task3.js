ad2b5563363a468ac3926353a23fcb618731406d
Bharat Pratap Singh <bharatpratapsingh18@gmail.com>
2026-09-16 08:56:35 +0530

    ordering

--- lab1/EventLoop/task3.js
+++ lab1/EventLoop/task3.js
@@ -4,3 +4,4 @@ setTimeout(() => console.log("2: setTimeout"), 0);
 setImmediate(() => console.log("3: setImmediate"));
 process.nextTick(() => console.log("4: nextTick"));
 console.log("4: end");
+    
