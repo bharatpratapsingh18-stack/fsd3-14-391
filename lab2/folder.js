b6e9cbdd26346fa26de75b297eb63510023dbfb2
Bharat Pratap Singh <bharatpratapsingh18@gmail.com>
2026-09-16 09:00:16 +0530

    folder

--- lab2/folder.js
+++ lab2/folder.js
@@ -10,4 +10,4 @@ import {mkdir , rm} from "fs/promises";
 // await rm("docs/resumes/data",{recursive: true});
 
 //removes main folder and sub folder alss
-await rm("docs",{recursive: true});
+await rm("docs",{recursive: true});     
