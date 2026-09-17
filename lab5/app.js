d6a993c42ee37b6ead4db337d17997d1e0ed6cf9
Bharat Pratap Singh <bharatpratapsingh18@gmail.com>
2026-09-17 13:59:40 +0530

    basic commit

--- lab5/app.js
+++ lab5/app.js
@@ -1,11 +1,23 @@
-import express from 'express'
+import express from "express";
 
-const app =express();
-app.get("",(req,res)=>{
-    res.send("<h1>Hello Express");
+const app = express();
+app.get("/", (req, res) => {
+  res.send("<h1>Hello Express");
 });
-app.use("*",(req,res)=>{
-    res.send("route not found");
-})
+app.get("/about", (req, res) => {
+  res.send("We are FSD Developer");
+});
+app.post("/login", (req, res) => {
+  res.send({ msg: "user login" });
+});
+app.put("user/update/1", (req, res) => {
+  res.send({ msg: "user update" });
+});
+app.delete("/user/1", (req, res) => {
+  res.send({ msg: "remove user 1" });
+});
+app.use((req, res) => {
+  res.status(400).send("NOT Found");
+});
 
-app.listen(3000,()=>console.log("server is Running"));
+app.listen(3333, () => console.log("server is Running at 3333"));
